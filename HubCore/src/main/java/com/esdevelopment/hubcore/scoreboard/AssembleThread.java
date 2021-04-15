e46e8e50c1d5903da0e59453393d812a94fc9b91
Saif Sharof <67766034+SaifSharof@users.noreply.github.com>
2021-04-15 19:21:24 +0300

    AssembleThread update

--- HubCore/src/main/java/com/esdevelopment/hubcore/scoreboard/AssembleThread.java
+++ HubCore/src/main/java/com/esdevelopment/hubcore/scoreboard/AssembleThread.java
@@ -40,7 +40,6 @@ public class AssembleThread extends Thread {
             try {
                 AssembleBoard board = this.assemble.getBoards().get(player.getUniqueId());
 
-                // This shouldn't happen, but just in case
                 if (board == null) {
                     continue;
                 }
@@ -53,11 +52,8 @@ public class AssembleThread extends Thread {
                     continue;
                 }
 
-                // Just make a variable so we don't have to
-                // process the same thing twice
                 String title = ChatColor.translateAlternateColorCodes('&', this.assemble.getAdapter().getTitle(player));
 
-                // Update the title if needed
                 if (!objective.getDisplayName().equals(title)) {
                     objective.setDisplayName(title);
                 }
@@ -65,7 +61,6 @@ public class AssembleThread extends Thread {
                 List<String> newLines = this.assemble.getAdapter().getLines(player);
 
 
-                // Allow adapter to return null/empty list to display nothing
                 if (newLines == null || newLines.isEmpty()) {
                     board.getEntries().forEach(AssembleBoardEntry::remove);
                     board.getEntries().clear();
@@ -74,12 +69,10 @@ public class AssembleThread extends Thread {
                         newLines = this.assemble.getAdapter().getLines(player).subList(0, 15);
                     }
 
-                    // Reverse the lines because scoreboard scores are in descending order
                     if (!this.assemble.getAssembleStyle().isDecending()) {
                         Collections.reverse(newLines);
                     }
 
-                    // Remove excessive amount of board entries
                     if (board.getEntries().size() > newLines.size()) {
                         for (int i = newLines.size(); i < board.getEntries().size(); i++) {
                             AssembleBoardEntry entry = board.getEntryAtPosition(i);
@@ -90,22 +83,16 @@ public class AssembleThread extends Thread {
                         }
                     }
 
-                    // Update existing entries / add new entries
                     int cache = this.assemble.getAssembleStyle().getStartNumber();
                     for (int i = 0; i < newLines.size(); i++) {
                         AssembleBoardEntry entry = board.getEntryAtPosition(i);
 
-                        // Translate any colors
                         String line = ChatColor.translateAlternateColorCodes('&', newLines.get(i));
 
-                        // If the entry is null, just create a new one.
-                        // Creating a new AssembleBoardEntry instance will add
-                        // itself to the provided board's entries list.
                         if (entry == null) {
                             entry = new AssembleBoardEntry(board, line, i);
                         }
 
-                        // Update text, setup the team, and update the display values
                         entry.setText(line);
                         entry.setup();
                         entry.send(
