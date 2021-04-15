0720ff7e93b0ef3a4e00a6cd1bebd0756fb03c56
Enoughdv <67561225+Enoughsdv@users.noreply.github.com>
2021-04-15 11:56:49 -0500

    Update ScoreboardAdapter.java

--- HubCore/src/main/java/com/esdevelopment/hubcore/scoreboard/adapter/ScoreboardAdapter.java
+++ HubCore/src/main/java/com/esdevelopment/hubcore/scoreboard/adapter/ScoreboardAdapter.java
@@ -2,12 +2,16 @@ package com.esdevelopment.hubcore.scoreboard.adapter;
 
 
 import com.esdevelopment.hubcore.HubCore;
+import com.esdevelopment.hubcore.features.Ping;
 import com.esdevelopment.hubcore.scoreboard.*;
 import com.esdevelopment.hubcore.util.CC;
 import com.esdevelopment.hubcore.util.PlayerCountThread;
 import org.bukkit.entity.Player;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.Bukkit;
 
 public class ScoreboardAdapter implements AssembleAdapter {
 
@@ -18,6 +22,32 @@ public class ScoreboardAdapter implements AssembleAdapter {
 
     @Override
     public List<String> getLines(Player player) {
-        return PlaceholderAPI.setPlaceholders(player, HubCore.get().getConfig().getStringList("SCOREBOARD.LINES"));
+        List<String> list = new ArrayList<String>();
+        
+        if(Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")){
+            for(String lines : HubCore.get().getConfig().getStringList("SCOREBOARD.LINES").stream()
+                    
+                    .map(a -> a.replace("{online}", String.valueOf(PlayerCountThread.PLAYER_COUNT)))
+                    .map(a -> a.replace("{ping}", String.valueOf(Ping.getPing(player))))
+                    
+                    .collect(Collectors.toList())) {
+                
+                        list.add(PlaceholderAPI.setPlaceholders(player, lines));
+                    }
+        } else {
+            
+            for(String lines : HubCore.get().getConfig().getStringList("SCOREBOARD.LINES").stream()
+                    
+                    .map(a -> a.replace("{online}", String.valueOf(PlayerCountThread.PLAYER_COUNT)))
+                    .map(a -> a.replace("{ping}", String.valueOf(Ping.getPing(player))))
+                    
+                    .collect(Collectors.toList())) {
+                
+                        list.add(lines);
+                    }
+            
+        }
+        
+        return list;
     }
 }
