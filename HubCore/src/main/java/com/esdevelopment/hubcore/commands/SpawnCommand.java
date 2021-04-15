362e7eb58efdb95b51850324d55ba0458db5d5f2
Enoughdv <67561225+Enoughsdv@users.noreply.github.com>
2021-04-15 16:31:35 -0500

    Add files via upload

---
+++ HubCore/src/main/java/com/esdevelopment/hubcore/commands/SpawnCommand.java
@@ -0,0 +1,38 @@
+package com.esdevelopment.hubcore.commands;
+
+import com.esdevelopment.hubcore.HubCore;
+import com.esdevelopment.hubcore.util.CC;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SpawnCommand implements CommandExecutor {
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command cannot be executed from console");
+        }
+
+        Player player = (Player) sender;
+
+        if (!player.hasPermission("hubcore.spawn")) {
+            sender.sendMessage(CC.translate("&cNo Permission."));
+        }
+        
+        if(HubCore.get().getConfig().getString("Spawn.location") == null){
+            player.sendMessage(CC.translate("&cSpawn no setted"));
+        }
+        
+        player.sendMessage(CC.translate(HubCore.get().getConfig().getString("Spawn.send.message")));
+        player.playSound(player.getLocation(),
+                Sound.valueOf(HubCore.get().getConfig().getString("Spawn.send.sound")),
+                1.0f,
+                1.0f);
+        
+        return false;
+    }
+}
