00cb3cd96c0668d13f1db72001b971a121f8deb6
Enoughdv <67561225+Enoughsdv@users.noreply.github.com>
2021-04-15 11:13:08 -0500

    Updates ScoreboardAdapter
    Added support for PAPI and configurable lines

--- HubCore/src/main/java/com/esdevelopment/hubcore/scoreboard/adapter/ScoreboardAdapter.java
+++ HubCore/src/main/java/com/esdevelopment/hubcore/scoreboard/adapter/ScoreboardAdapter.java
@@ -18,17 +18,6 @@ public class ScoreboardAdapter implements AssembleAdapter {
 
     @Override
     public List<String> getLines(Player player) {
-        final List<String> lines = new ArrayList<>();
-
-        lines.add(CC.translate("&7&m-------------------"));
-        lines.add(CC.translate("&cOnline: &f"));
-        lines.add(CC.translate("&f" + PlayerCountThread.PLAYER_COUNT));
-        lines.add("");
-        lines.add(CC.translate(HubCore.get().getConfig().getString("SCOREBOARD.MIDDLE")));
-        lines.add(CC.translate(""));
-        lines.add(CC.translate(""));
-        lines.add(CC.translate(HubCore.get().getConfig().getString("SCOREBOARD.FOOTER")));
-        lines.add(CC.translate("&7&m-------------------"));
-        return lines;
+        return PlaceholderAPI.setPlaceholders(player, HubCore.get().getConfig().getStringList("SCOREBOARD.LINES"));
     }
 }
