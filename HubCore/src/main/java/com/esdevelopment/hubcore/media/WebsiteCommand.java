ee186620513581e8894b215ddfe438d7459c9b65
Saif Sharof <67766034+SaifSharof@users.noreply.github.com>
2021-04-16 00:31:14 +0300

    Added Media commands

---
+++ HubCore/src/main/java/com/esdevelopment/hubcore/media/WebsiteCommand.java
@@ -0,0 +1,35 @@
+package com.esdevelopment.hubcore.media;
+
+import com.esdevelopment.hubcore.HubCore;
+import com.esdevelopment.hubcore.util.CC;
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class WebsiteCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command cannot be executed from console");
+        }
+
+        final Player player = (Player) sender;
+
+        if (!player.hasPermission("hubcore.media")) {
+            player.sendMessage(CC.translate("&cNo Permission"));
+        }
+
+        if(Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")){
+            player.sendMessage(CC.translate(HubCore.get().getConfig().getString("MEDIA_LINKS.WEBSITE").replace("%bullet_point%", "•")));
+        } else {
+            for(String list : HubCore.get().getConfig().getStringList("")){
+                player.sendMessage(CC.translate(list).replace("%bullet_point%", "•"));
+            }
+        }
+        return true;
+    }
+}
