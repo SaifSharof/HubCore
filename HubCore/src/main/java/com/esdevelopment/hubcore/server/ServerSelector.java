48e7b5388e6ffeaeac3a6b74d6e2d4a94811a5c7
Saif Sharof <67766034+SaifSharof@users.noreply.github.com>
2021-04-17 00:06:56 +0300

    Updated

--- HubCore/src/main/java/com/esdevelopment/hubcore/server/ServerSelector.java
+++ HubCore/src/main/java/com/esdevelopment/hubcore/server/ServerSelector.java
@@ -5,6 +5,7 @@ import com.esdevelopment.hubcore.HubCore;
 import com.esdevelopment.hubcore.util.*;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -20,7 +21,7 @@ import java.util.stream.IntStream;
 public class ServerSelector implements Listener {
 
     private final String bullet_point = "•", right_arrow = "»", left_arrow = "«";
-
+    FileConfiguration config = HubCore.get().getConfig();
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
@@ -30,24 +31,25 @@ public class ServerSelector implements Listener {
 
         } else {
 
-            if (CC.translate(HubCore.get().getConfig().getString("ITEM.SERVER_SELECTOR.NAME")).equalsIgnoreCase(event.getItem().getItemMeta().getDisplayName())) {
-                Inventory inventory = Bukkit.createInventory(null, HubCore.get().getConfig().getInt("SERVER_SELECTOR.SIZE"), CC.translate(HubCore.get().getConfig().getString("SERVER_SELECTOR.NAME")));
+            if (CC.translate(config.getString("ITEM.SERVER_SELECTOR.NAME")).equalsIgnoreCase(event.getItem().getItemMeta().getDisplayName())) {
+                Inventory inventory = Bukkit.createInventory(null, config.getInt("SERVER_SELECTOR.SIZE"), CC.translate(config.getString("SERVER_SELECTOR.NAME")));
                 player.openInventory(inventory);
-                IntStream.range(0, HubCore.get().getConfig().getInt("SERVER_SELECTOR.SIZE")).forEach(i -> inventory.setItem(i, new ItemBuilder(Material.getMaterial(
-                        HubCore.get().getConfig().getString("GLASS_PANE.MATERIAL")), HubCore.get().getConfig().getInt("GLASS_PANE.AMOUNT"), (short) HubCore.get().getConfig().getInt("GLASS_PANE.VALUE"))
-                        .setName(HubCore.get().getConfig().getString("GLASS_PANE.NAME"))
+                IntStream.range(0, config.getInt("SERVER_SELECTOR.SIZE")).forEach(i -> inventory.setItem(i, new ItemBuilder(Material.getMaterial(
+                        config.getString("GLASS_PANE.MATERIAL")), config
+                        .getInt("GLASS_PANE.AMOUNT"), (short) config.getInt("GLASS_PANE.VALUE"))
+                        .setName(config.getString("GLASS_PANE.NAME"))
                         .create()));
 
-                List<String> practice_lore = new ArrayList<>();
-                HubCore.get().getConfig().getStringList("SERVER_SELECTOR.SERVERS.1.LORE").forEach(string -> practice_lore.add(CC.translate(string
+                List<String> server_lore = new ArrayList<>();
+                config.getStringList("SERVER_SELECTOR.SERVERS.1.LORE").forEach(string -> server_lore.add(CC.translate(string
                         .replace("%bullet_point%", bullet_point))
                         .replace("%right_arrow%", right_arrow)
                         .replace("%left_arrow%", left_arrow)));
-                ItemStack practice = new ItemBuilder(Material.getMaterial(HubCore.get().getConfig().getString("SERVER_SELECTOR.SERVERS.1.MATERIAL")))
-                        .setName(CC.translate(HubCore.get().getConfig().getString("SERVER_SELECTOR.SERVERS.1.NAME")))
-                        .setLore(practice_lore)
+                ItemStack practice = new ItemBuilder(Material.getMaterial(config.getString("SERVER_SELECTOR.SERVERS.1.MATERIAL")))
+                        .setName(CC.translate(config.getString("SERVER_SELECTOR.SERVERS.1.NAME")))
+                        .setLore(server_lore)
                         .create();
-                inventory.setItem(HubCore.get().getConfig().getInt("SERVER_SELECTOR.SERVERS.1.SLOT"), practice);
+                inventory.setItem(config.getInt("SERVER_SELECTOR.SERVERS.1.SLOT"), practice);
 
                 player.updateInventory();
             }
@@ -58,9 +60,11 @@ public class ServerSelector implements Listener {
     public void onClick(InventoryClickEvent event) {
         Player player = (Player) event.getWhoClicked();
 
-        if (event.getInventory() != null && CC.translate(HubCore.get().getConfig().getString("SERVER_SELECTOR.NAME")).equalsIgnoreCase(event.getInventory().getName())) {
-            if (CC.translate(HubCore.get().getConfig().getString("SERVER_SELECTOR.SERVERS.1.NAME")).equalsIgnoreCase(event.getCurrentItem().getItemMeta().getDisplayName())) {
-                BungeeUtil.sendToServer(player, HubCore.get().getConfig().getString("SERVER_SELECTOR.SERVERS.1.SERVER"));
+        if (event.getInventory() != null && CC.translate(config.getString("SERVER_SELECTOR.NAME")).equalsIgnoreCase(event.getInventory().getName())) {
+            if (CC.translate(config.getString("SERVER_SELECTOR.SERVERS.1.NAME")).equalsIgnoreCase(event.getCurrentItem().getItemMeta().getDisplayName())) {
+                BungeeUtil.sendToServer(player, config.getString("SERVER_SELECTOR.SERVERS.1.SERVER"));
+
+                if (CC.translate(config.getString("SERVER_SELECTOR.SERVERS.1.NAME")).equalsIgnoreCase(String.valueOf(event.getCurrentItem().getItemMeta().getDisplayName() == null))) event.setCancelled(true);
             }
         }
     }
